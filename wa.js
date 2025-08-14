fd46901c5ebf939b3a1e625b9a6ffb71f41f1c8f
John Cleary <johnmartincleary@gmail.com>
2025-08-14 21:06:24 +0545

    extract methods to make code easier to read

--- wa.js
+++ wa.js
@@ -1,4 +1,6 @@
 const { create } = require('@open-wa/wa-automate');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 create({
   headless: true, // Set to false if you want to see the browser
@@ -8,45 +10,54 @@ create({
 }).then(client => start(client));
 
 function start(client) {
-  const fs = require('fs');
-  const yaml = require('js-yaml');
-
-  const config = yaml.load(fs.readFileSync('config.yaml', 'utf8'));
-  const bins = config.bins.map(bin => ({
-    ...bin,
-    startDate: new Date(bin.startDate)
-  }));
-
-  // Get next bin day
-  const binDay = new Date();
-  while (binDay.getDay() !== config.bin_day_of_week) {
-    binDay.setDate(binDay.getDate() + 1);
-  }
-
-  // Find which bins are due this week
-  const dueBins = bins.filter(bin => {
-    // Calculate weeks since the start date
-    const diffTime = Math.abs(binDay - bin.startDate);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    const diffWeeks = Math.floor(diffDays / 7);
-
-    // If the number of weeks is divisible by the cycle, the bin is due
-    return diffWeeks % bin.cycleWeeks === 0;
-  });
-
-  // Create a message based on which mins are due
-  let message = '';
-  if (dueBins.length > 0) {
-    message += "Bins for collection this week (" + binDay.toDateString() + ") :\n\n";
-    message += dueBins.map(bin => bin.color).join('\n\n');
-  } else {
-    message += "No bins scheduled for collection this week.";
-  }
-
-  message += "\n\nMore info at: " + config.website_url;
+  const config = loadConfig();
+  const bins = parseBins(config.bins);
+  const binDay = getNextBinDay(config.bin_day_of_week);
+  const dueBins =getDueBins(bins, binDay);
+  const message = createBinMessage(dueBins, binDay, config);
 
   client.sendText(config.bin_chat, message);
   console.log('Message Sent');
   console.log(message);
 }
+
+function loadConfig() {
+  return yaml.load(fs.readFileSync('config.yaml', 'utf8'));
+}
+
+function parseBins(binsConfig) {
+  return binsConfig.map(bin => ({
+    ...bin,
+    startDate: new Date(bin.startDate)
+  }));
+}
+
+function getNextBinDay(targetDay) {
+  const date = new Date();
+  while (date.getDay() !== targetDay) {
+    date.setDate(date.getDate() + 1);
+  }
+  return date;
+}
+
+function getDueBins(bins, binDay) {
+  return bins.filter(bin => {
+    const diffTime = Math.abs(binDay - bin.startDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    const diffWeeks = Math.floor(diffDays / 7);
+    return diffWeeks % bin.cycleWeeks === 0;
+  });
+}
+
+function createBinMessage(dueBins, binDay, config) {
+
+  if (dueBins.length === 0) {
+    return `No bins scheduled for collection this week.\n\nMore info at: ${config.website_url}`;
+  }
+
+  const binColors = dueBins.map(bin => bin.color).join('\n\n');
+  return `Bins for collection this week (${binDay.toDateString()}) :\n\n${binColors}\n\nMore info at: ${config.website_url}`;
+}
+
+
 
